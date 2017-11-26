1a3c33d9affbd8b39d8742183c1d4b81202c7feb
DriverMaple <763638494@qq.com>
2017-11-27 00:03:47 +0800

    as

--- guideserver/src/main/java/com/maple/guideserver/GuideserverApplication.java
+++ guideserver/src/main/java/com/maple/guideserver/GuideserverApplication.java
@@ -2,11 +2,18 @@ package com.maple.guideserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GuideserverApplication {
+public class GuideserverApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuideserverApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GuideserverApplication.class);
 	}
 }
